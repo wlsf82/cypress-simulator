b4971cc0d0b48a45ba4f9ef952693bcb008ada78
Walmyr <wlsf82@gmail.com>
2024-12-30 01:17:32 +0100

    Delete duplicated tests
    To speed tests' execution time.

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -10,36 +10,6 @@ describe("Cypress Simulator", options, () => {
     cy.visit("./src/index.html")
   })
 
-  it("successfully runs the code", () => {
-    cy.get("textarea").type("cy.log('Yo!')")
-    cy.contains("button", "Run").click()
-    cy.tick(2000)
-
-    cy.get("#outputArea")
-      .should("contain", "Success:")
-      .and("contain", "cy.log('Yo!') // Logged message 'Yo!'")
-  })
-
-  it("shows warning for not implemented Cypress code", () => {
-    cy.get("textarea").type("cy.contains('Hello, World!')")
-    cy.contains("button", "Run").click()
-    cy.tick(2000)
-
-    cy.get("#outputArea")
-      .should("contain", "Warning:")
-      .and("contain", "The `cy.contains` command has not been implemented yet.")
-  })
-
-  it("shows error for invalid Cypress code", () => {
-    cy.get("textarea").type("console.log('Hello, World!')")
-    cy.contains("button", "Run").click()
-    cy.tick(2000)
-
-    cy.get("#outputArea")
-      .should("contain", "Error:")
-      .and("contain", "Invalid Cypress command: console.log('Hello, World!')")
-  })
-
   it("shows error for valid Cypress code without parentheses", () => {
     cy.get("textarea").type("cy.get")
     cy.contains("button", "Run").click()
@@ -48,20 +18,6 @@ describe("Cypress Simulator", options, () => {
     cy.get("#outputArea")
       .should("contain", "Error:")
       .and("contain", "Missing parentheses on `cy.get` command")
-  })
-
-  it("answers for help", () => {
-    cy.get("textarea").type("help")
-    cy.contains("button", "Run").click()
-    cy.tick(2000)
-
-    cy.get("#outputArea")
-      .should("contain", "Common Cypress commands and examples:")
-      .and("contain","For more commands and details, visit the official Cypress API documentation.")
-    cy.contains("#outputArea a", "official Cypress API documentation")
-      .should("have.attr", "href", "https://docs.cypress.io/api/table-of-contents")
-      .and("have.attr", "target", "_blank")
-      .and("have.attr", "rel", "noopener noreferrer")
   })
 
   it("checks the run button disabled and enabled states", () => {
@@ -74,35 +30,6 @@ describe("Cypress Simulator", options, () => {
     cy.get("textarea").clear()
 
     cy.contains("button", "Run").should("be.disabled")
-  })
-
-  it("checks the run button running state", () => {
-    cy.get("textarea").type("cy.log('Yo!')")
-
-    cy.contains("button", "Run").click()
-
-    cy.contains("button", "Running...").should("be.visible")
-
-    cy.tick(2000)
-
-    cy.contains("button", "Running...").should("not.exist")
-    cy.contains("button", "Run").should("be.visible")
-  })
-
-  it("shows and hides the logout button", () => {
-    cy.get("#sandwich-menu").click()
-    cy.contains("button", "Logout").should("be.visible")
-
-    cy.get("#sandwich-menu").click()
-    cy.contains("button", "Logout").should("not.be.visible")
-  })
-
-  it("successfully logs out", () => {
-    cy.get("#sandwich-menu").click()
-    cy.contains("button", "Logout").click()
-
-    cy.contains("button", "Login").should("be.visible")
-    cy.get("#sandwich-menu").should("not.be.visible")
   })
 
   it("clears the code input when logging off then logging in again", () => {
@@ -137,19 +64,5 @@ describe("Cypress Simulator", options, () => {
     cy.contains("button", "Login").click()
 
     cy.get("textarea").should("have.value", "")
-  })
-
-  it("maximizes and minimizes the code output", () => {
-    cy.get("textarea").type("help")
-    cy.contains("button", "Run").click()
-    cy.tick(2000)
-
-    cy.get(".expand-collapse").click()
-
-    cy.get("#collapseIcon").should("be.visible")
-
-    cy.get(".expand-collapse").click()
-
-    cy.get("#expandIcon").should("be.visible")
   })
 })
